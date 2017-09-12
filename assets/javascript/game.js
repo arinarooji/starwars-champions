74833e480fe74df005d3f065024f57772738ea5a
Keivon Arooji <karooji0@email.cpcc.edu>
2017-09-12 16:41:39 -0400

    linked files

---
+++ assets/javascript/game.js
@@ -0,0 +1,5 @@
+console.log("Hi! JavaScript is linked");
+
+//The first goal is to get the selection process working/displaying properly
+//Create variables/arrays to store all characters, playerCharacter, opponentCharacter
+//Characters can be an object, each slightly modified.
