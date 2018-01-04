0cbb5ed614680a16b576308b35c66e7e21e9cba2
Keivon Arooji <karooji0@email.cpcc.edu>
2018-01-04 14:05:49 -0500

    Added player/enemy objects

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,14 +11,10 @@ $(document).ready(function(){
 	//Reference all character instances
 	var characters = [vader, yoda, kylo, rey, luke];
 
-	//Global player/enemy variables
-	var player, playerHealth;
-	var enemy, enemyHealth;
-	var wins = 0, losses = 0;
-	var championWins = 0;
-	var playerMultiplier = 1.00;
-	var champion = false;
-	var playerSelected = false;
+	//Player and enemy objects
+	var player = { health: 100, wins: 0, losses: 0, champWins: 0, isSelected: false, isChampion: false, multiplier: 1.00 };
+	var enemy  = { health: 100 };
+
 	
 	//Hide attack buttons at start
 	$("#heavy-button, #light-button").hide();
@@ -49,31 +45,31 @@ $(document).ready(function(){
 		for (var i = 0; i < characters.length; i++) {
 			
 			//If the selected element has player class, id equals iterated character id, and the player IS NOT selected...
-			if ($(this).hasClass("player") && $(this).attr("id") === characters[i].id && !playerSelected) {
+			if ($(this).hasClass("player") && $(this).attr("id") === characters[i].id && !player.isSelected) {
 				
-				//The player has now selected a character
-				player = characters[i];
-				playerHealth = player.health;
-				playerSelected  = true;
+				//Reference the selected character object in player.character
+				player.character  = characters[i];
+				player.health     = player.character.health;
+				player.isSelected = true;
 
 				//Update the DOM (name, saber inventory, health bar, hide carousel-1 controls)
-				$("#playerName").html(player.id);
-				$("#SABERS").html("LIGHTSABER: " + player.sabers);
-				$(".playerHealth").css("width", playerHealth + "%");
+				$("#playerName").html(player.character.id);
+				$("#SABERS").html("LIGHTSABER: " + player.character.sabers);
+				$(".playerHealth").css("width", player.health + "%");
 				$("#carousel-1-prev, #carousel-1-next, #indicators-1").hide();
 				$(saberOn).trigger('play'); //Play the saberOn sound
 			}
 
 			//If the selected id equals current character id, the player id is not identical, and player IS selected...
-			else if ($(this).hasClass("enemy") && $(this).attr("id") === characters[i].id && $(this).attr("id") !== player.id && playerSelected) {
+			else if ($(this).hasClass("enemy") && $(this).attr("id") === characters[i].id && player.isSelected && $(this).attr("id") !== player.character.id) {
 				
 				//The enemy is now the selected character
-				enemy = characters[i];
-				enemyHealth = enemy.health;
+				enemy.character = characters[i];
+				enemy.health    = enemy.character.health;
 
 				//Update DOM (name, health bar, hide carousel-2 controls, show attack btns)
-				$("#enemyName").html(enemy.id);
-				$(".enemyHealth").css("width", enemyHealth + "%");
+				$("#enemyName").html(enemy.character.id);
+				$(".enemyHealth").css("width", enemy.health + "%");
 				$("#carousel-2-prev, #carousel-2-next, #indicators-2").hide();
 				$("#heavy-button, #light-button").show();
 				$(saberOn2).trigger('play'); //Play saberOn2 sound
@@ -87,9 +83,9 @@ $(document).ready(function(){
 		$(heavySabers[heavySounds]).trigger('play');
 		(heavySounds < 2)? heavySounds++ : heavySounds = 0
 
-		enemyHealth  -= (Math.floor(Math.random() * player.attack)) * playerMultiplier;
-		playerHealth -= Math.floor(Math.random() * enemy.attack);
-		UpdateHealthBars(playerHealth, enemyHealth);
+		enemy.health  -= (Math.floor(Math.random() * player.character.attack)) * player.multiplier;
+		player.health -= Math.floor(Math.random() * enemy.character.attack);
+		UpdateHealthBars(player.health, enemy.health);
 	});
 
 	//LIGHT ATTACK
@@ -98,58 +94,58 @@ $(document).ready(function(){
 		$(lightSabers[lightSounds]).trigger('play');
 		(lightSounds < 2)? lightSounds++ : lightSounds = 0
 
-		enemyHealth  -= (Math.floor(Math.random() * player.lightAttack)) * playerMultiplier;
-		playerHealth -= Math.floor(Math.random() * enemy.lightAttack);
-		UpdateHealthBars(playerHealth, enemyHealth);
+		enemy.health  -= (Math.floor(Math.random() * player.character.lightAttack)) * player.multiplier;
+		player.health -= Math.floor(Math.random() * enemy.character.lightAttack);
+		UpdateHealthBars(player.health, enemy.health);
 	});
 
 	//Check health levels on every button click
 	$(".btn").on("click", function(){
 		
 		//If enemy defeated
-		if (enemyHealth <= 0) {
+		if (enemy.health <= 0) {
 			
 			//Is the player a champion? Add to championWins. Else, add to wins
-			(champion)? $("#CHAMPWINS").html("CHAMPION WINS: " + (championWins++)) : wins++
-			playerMultiplier += 0.05; //Increase playerMultiplier
-			playerHealth = player.health; //Refill playerHealth
+			(player.isChampion)? $("#CHAMPWINS").html("CHAMPION WINS: " + (player.champWins++)) : player.wins++
+			player.multiplier += 0.05; //Increase player.multiplier
+			player.health = player.character.health; //Refill playerHealth
 
 			//Update DOM
 			$("#enemyName").html("OPPONENT");
 			$("#heavy-button, #light-button").hide();
 			$("#carousel-2-prev, #carousel-2-next, #indicators-2").show();
-			$("#WINS").html("WINS: " + wins);
-			UpdateHealthBars(playerHealth, enemyHealth);
+			$("#WINS").html("WINS: " + player.wins);
+			UpdateHealthBars(player.health, enemy.health);
 
 			//Check if the enemy's lightsaber is in player inventory
-			if (player.sabers.indexOf(enemy.sabers[0]) == -1) {
-				player.sabers.push(enemy.sabers[0]);
-				$("#SABERS").html("LIGHTSABERS: " + player.sabers);
+			if (player.character.sabers.indexOf(enemy.character.sabers[0]) == -1) {
+				player.character.sabers.push(enemy.character.sabers[0]);
+				$("#SABERS").html("LIGHTSABERS: " + player.character.sabers);
 			}
 
 			//Check if all lightsabers acquired
-			if (player.sabers.length === characters.length && !champion) {
+			if (player.character.sabers.length === characters.length && !player.isChampion) {
 				alert("YOU ARE THE GALACTIC CHAMPION!");
-				champion = true;
+				player.isChampion = true;
 			}
 		}
 
 		//If player defeated
-		if (playerHealth <= 0) {
+		if (player.health <= 0) {
 			
 			//Update selection, champion status, multiplier, losses
-			playerSelected   = false, champion = false;
-			playerMultiplier = 1.00;
-			losses += 1;
+			player.isSelected = false, player.isChampion = false;
+			player.multiplier  = 1.00;
+			player.losses += 1;
 
 			//Remove all acquired sabers and update the inventory
-			player.sabers.length = 1;
-			$("#SABERS").html("LIGHTSABER: " + player.sabers);
+			player.character.sabers.length = 1;
+			$("#SABERS").html("LIGHTSABER: " + player.character.sabers);
 
 			//Update the DOM (names, losses, hide attack btns, health bars, show carousel controls)
 			$("#playerName").html("PLAYER");
 			$("#enemyName").html("OPPONENT");
-			$("#LOSSES").html("LOSSES: " + losses);
+			$("#LOSSES").html("LOSSES: " + player.losses);
 			$("#heavy-button, #light-button").hide();
 			$("#carousel-1-prev, #carousel-1-next, #indicators-1, #carousel-2-prev, #carousel-2-next, #indicators-2").show();
 		}
